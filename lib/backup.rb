901a9f2c0c3e3de9cec38c1bf6e2d2beb69fa40c
Heber Lazo <heberlazo@gmail.com>
2024-02-21 14:41:10 -0500

    Added locally

--- lib/backup.rb
+++ lib/backup.rb
@@ -13,21 +13,27 @@ app_dir = ENV['APP_DIR']
 backup_dir = ENV['BACKUP_DIR']
 s3_bucket = ENV['S3_BUCKET']
 s3_region = ENV['S3_REGION']
+backup_destination = ENV['INPUT_BACKUP_DESTINATION'].downcase
 
 begin
-  db_backup = DatabaseBackup.new(db_name, db_user, db_password, db_host, backup_dir)
-  app_backup = AppBackup.new(app_dir, backup_dir)
-  s3_uploader = S3Uploader.new(s3_bucket, s3_region)
+  db_backup_path = DatabaseBackup.new(db_name, db_user, db_password, db_host, backup_dir).backup
+  app_backup_path = AppBackup.new(app_dir, backup_dir).backup
 
-  db_backup_path = db_backup.backup
-  app_backup_path = app_backup.backup
+  case backup_destination
+  when 's3'
+    raise ArgumentError, 'S3 bucket and region must be provided for S3 backup' if s3_bucket.empty? || s3_region.empty?
 
-  s3_uploader.upload(db_backup_path)
-  s3_uploader.upload(app_backup_path)
+    S3Uploader.new(s3_bucket, s3_region).upload(db_backup_path)
+    S3Uploader.new(s3_bucket, s3_region).upload(app_backup_path)
+  when 'local'
+    puts "Backups saved locally in #{backup_dir}"
+  else
+    raise ArgumentError, 'Invalid backup destination. Use "s3" or "local".'
+  end
 
-  Cleanup.run(db_backup_path, app_backup_path)
+  Cleanup.run(db_backup_path, app_backup_path) if backup_destination == 's3'
 
   puts 'Backup and upload completed successfully.'
 rescue StandardError => e
-  puts "Error: #{e.message}"
+  puts "Error during backup process: #{e.message}"
 end
